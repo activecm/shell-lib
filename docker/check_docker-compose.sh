779f65d73f59cba269a0b5c3806ec62a8200ddb1
Logan L <logan@bhis.co>
2023-01-25 18:34:56 -0700

    Update docker-compose to v1.29.2

--- docker/check_docker-compose.sh
+++ docker/check_docker-compose.sh
@@ -13,7 +13,7 @@ fi
 MIN_VERSION_MAJOR=1
 MIN_VERSION_MINOR=16
 MAX_VERSION_MAJOR=1
-MAX_VERSION_MINOR=25
+MAX_VERSION_MINOR=29
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
 source ../acmlib.sh
